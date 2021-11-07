0a3d486a1839aa38653b57ace80c184f56c484d0
kondors1995 <normandija1945@gmail.com>
2021-11-07 06:37:21 +0000

    sm8250-common: Add Livedisplay 2.1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,9 +98,8 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_fm.xml
 endif
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml \
+        vendor/evolution/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_SKUS += nfc
